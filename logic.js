1598e607037d1da11674be658cb32352cf5deb53
Joanna Preston <joannappreston@gmail.com>
2020-03-19 13:45:17 -0700

    Pseudocoded JavaScript file

---
+++ logic.js
@@ -0,0 +1,42 @@
+// ********** STEPS ************* //
+
+// Create an object that holds quiz questions, answers, and correct answer
+
+// Correlate quiz questions and answers with html
+
+// Define an initialize function that starts the program over
+    // Starts with intro card
+    // Score resets to zero
+    // if button is clicked call quiz starts function
+
+// Define a timer function
+    // Countdown from 75 seconds
+    // Subtracts 5 seconds if user answers question wrong
+    // When time reaches 0 call game over function
+
+// Define a function that starts the quiz questions when user clicks start quiz button
+    // Pulls up first question card
+    // Timer starts (calls timer function)
+
+// Define a function for if answer is correct: correctAnswer
+    // Display "correct"
+    // Add to score
+    // Next question
+
+// Define a function for if answer is incorrect: wrongAnswer
+    // Display "incorrect"
+    // Subract 5 seconds from the timer
+
+// Define a function that checks if the user answered the question correctly
+    // If correct display correctAnswer function
+    // If incorrect display wrongAnswer function
+
+// Define a game over function
+    // Called if all questions are answered 
+    // OR called if timer reaches zero
+    // Display final score
+    // Allow user to saves initials and score
+
+// Call initialze function
+
+
