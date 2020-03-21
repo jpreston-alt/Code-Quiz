0e98dd9645dd6d34afbfc8a1598b8e99e93d101e
Joanna Preston <joannappreston@gmail.com>
2020-03-21 15:45:15 -0700

    Added function constructor and new instances of object Question

--- logic.js
+++ logic.js
@@ -1,8 +1,61 @@
 // ********** STEPS ************* //
 
-// Create an object that holds quiz questions, answers, and correct answer
+// Create an function constructor for an object that holds quiz questions, answers, and correct answer
+
+function Question(ask, answers, corrAns) {
+    this.ask = ask;
+    this.answers = answers;
+    this.corrAns = corrAns;
+};
+
+// Create object instances
+var q1 = new Question(
+    "What is Bug's favorite color?",
+    ["red", "blue", "green", "yellow"],
+    "green"
+);
+
+var q2 = new Question(
+    "What is Bug's favorite thing to do?",
+    ["eat", "sleep", "walk", "play"],
+    "sleep"
+);
+
+var q3 = new Question(
+    "What is Bug's favorite food?",
+    ["cucumbers and peanut butter", "carrots", "dog food", "treats"],
+    "cucumbers and peanut butter"
+);
+
+var qArr = [q1, q2, q3];
+
 
 // Correlate quiz questions and answers with html
+function askQuestion(question) {
+    document.getElementById("question").textContent = question.ask;
+    for (var i = 0; i < question.answers.length; i++) {
+        document.getElementById("ans" + (i + 1)).textContent = question.answers[i];
+    }
+};
+
+// Define a function that loops through each question and:
+    // Displays each question
+    // Checks if answer is correct
+function nextQuestion() {
+    for (var i = 1; i < qArr.length; i++) {
+        askQuestion(i);
+    }
+}
+
+// Define a function that checks if the user answered the question correctly
+    // If correct 
+        // Display "correct"
+        // Add to score
+        // Next question
+    // If incorrect
+        // Display "incorrect"
+        // Subract 5 seconds from the timer
+
 
 // Create a score variable to hold quiz score
 var totalScore;
@@ -17,8 +70,9 @@ function displayIntro() {
 
 // Define a function that displays quiz questions
 function startQuiz() {
-    contentDiv.innerHTML = '<div class="card text-left col-12 col-sm-10 col-md-9 col-lg-6"><div class="quiz-card"><h2 class="card-title">Question will go here.</h2><ul><li><button class="btn">A: Answer 1</button></li><li><button class="btn">B: Answer 2</button></li><li><button class="btn ">C: Answer 3</button></li><li><button class="btn">D: Answer 4</button></li></ul></div></div>'
+    contentDiv.innerHTML = '<div class="card text-left col-12 col-sm-10 col-md-9 col-lg-6"><div class="quiz-card"><h2 class="card-title" id="question">Question will go here.</h2><ul><li><button class="btn" id="ans1">A: Answer 1</button></li><li><button class="btn" id="ans2">B: Answer 2</button></li><li><button class="btn" id="ans3">C: Answer 3</button></li><li><button class="btn" id="ans4">D: Answer 4</button></li></ul></div></div>'
     document.body.appendChild(contentDiv);
+    askQuestion(q1);
 };
 
 
@@ -29,23 +83,6 @@ function init() {
     document.getElementById("start-btn").addEventListener("click", startQuiz);
 };
 
-
-// Define a function for if answer is correct: correctAnswer
-    // Display "correct"
-    // Add to score
-    // Next question
-
-// Define a function for if answer is incorrect: wrongAnswer
-    // Display "incorrect"
-    // Subract 5 seconds from the timer
-
-// Define a function that checks if the user answered the question correctly
-    // If correct display correctAnswer function
-    // If incorrect display wrongAnswer function
-
-// Define a function that loops through each question and:
-    // Displays each question
-    // Checks if answer is correct
 
 // Define a timer function
     // Starts when start quiz button is clicked
