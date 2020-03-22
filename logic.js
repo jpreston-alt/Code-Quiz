215cd7c9f744b8f4a02d580e0d984b6d1d8db8a2
Joanna Preston <joannappreston@gmail.com>
2020-03-21 17:35:45 -0700

    defined and called function to take user to next question if they answer previous question correctly

--- logic.js
+++ logic.js
@@ -1,11 +1,47 @@
-// ********** STEPS ************* //
 
-// Create a score variable to hold quiz score
+/*
+********** STEPS *************
+- Create an function constructor for an object that holds quiz questions, answers, and correct answer
+- Create global variables
+    -total score
+    -variables to hold DOM elements
+- Create object instances holding quiz questions
+- Create a function that displays quiz questions on the UI
+- Create a function that checks if the user answered the question correctly
+    - If correct
+        - Display "correct"
+        - Add to score
+        - Next question
+    - If incorrect
+        - Display "incorrect"
+        - Subract 5 seconds from the timer
+- Create a function that takes user to the next question and checks if it is correct
+- Create function that displays intro card and hides the quiz card
+- Create a function that starts the quiz by hiding the intro card and displaying the quiz card
+- Create an initialize function that starts the program over: 
+    - displays intro card
+    - resets score to zero
+    - when start button is clicked quiz is started by calling start quiz function
+- Create a timer function
+    - Starts when start quiz button is clicked
+    - Countdown from 75 seconds
+    - Subtracts 5 seconds if user answers question wrong
+    - When time reaches 0 call game over function
+- Create a game over function
+    - Called if all questions are answered
+    - OR called if timer reaches zero
+    - Display final score
+- High score
+    - high score clickable
+    - brings user to high score page
+    - allows user to save initials and score
+- Call initialize function to start program
+*/
+
 var totalScore;
 var quizCardEl = document.getElementById("quiz-card");
 var introCardEl = document.getElementById("intro-card");
 
-// Create an function constructor for an object that holds quiz questions, answers, and correct answer
 
 function Question(ask, answers, corrAns) {
     this.ask = ask;
@@ -13,7 +49,6 @@ function Question(ask, answers, corrAns) {
     this.corrAns = corrAns;
 };
 
-// Create object instances
 var q1 = new Question(
     "What is Bug's favorite color?",
     ["red", "blue", "green", "yellow"],
@@ -32,8 +67,10 @@ var q3 = new Question(
     "cucumbers and peanut butter"
 );
 
+// array of question object instances
+var qArr = [q1, q2, q3];
 
-// Correlate quiz questions and answers with html
+// displays questions on UI
 function displayQuestion(question) {
     document.getElementById("question").textContent = question.ask;
     for (var i = 0; i < question.answers.length; i++) {
@@ -41,26 +78,7 @@ function displayQuestion(question) {
     }
 };
 
-// Define a function that loops through each question and:
-    // Displays each question
-    // Checks if answer is correct
-function questionsLoop() {
-    var qArr = [q1, q2, q3];
-    for (var i = 0; i < qArr.length; i++) {
-        displayQuestion(i);
-    }
-}
-
-
-// Define a function that checks if the user answered the question correctly
-    // If correct 
-        // Display "correct"
-        // Add to score
-        // Next question
-    // If incorrect
-        // Display "incorrect"
-        // Subract 5 seconds from the timer
-
+// checks if answer is correct
 function checkCorrect(question) {
     quizCardEl.addEventListener("click", function (event) {
         event.preventDefault();
@@ -68,21 +86,33 @@ function checkCorrect(question) {
         if (event.target.matches("button")) {
             console.log(event.target.textContent)
             if (event.target.textContent === question.corrAns) {
-                displayCorrEl.textContent= "Correct!"
+                displayCorrEl.textContent = "Correct!";
+                totalScore++;
+                console.log(totalScore);
+                if ((qArr.indexOf(question) + 1) < qArr.length){
+                    nextQuestion(qArr[qArr.indexOf(question) + 1])
+                } else {
+                    console.log("quiz is over!")
+                }
             } else {
-                displayCorrEl.textContent= "Wrong!";
+                displayCorrEl.textContent = "Wrong!";
             }
-        } 
+        }
     });
-}
+};
 
+// takes the user to the next question, called in the checkCorrect function
+function nextQuestion(question) {
+    displayQuestion(question);
+    checkCorrect(question);
+};
 
-// Define a function that displays intro card
+// displays intro card, hides quiz card
 function displayIntro() {
     quizCardEl.classList.add("display-none"); 
 };
 
-// Define a function that starts quiz by displaying first quiz question
+// displays quiz card, hides intro card, starts quiz
 function startQuiz() {
     introCardEl.classList.add("display-none");
     quizCardEl.classList.remove("display-none");
@@ -90,29 +120,14 @@ function startQuiz() {
     checkCorrect(q1);
 };
 
-
-// Define an initialize function that starts the program over: displays intro card, resets score to zero, if start button is clicked quiz is started
+// defines initialize function
 function init() {
     displayIntro();
     totalScore = 0;
     document.getElementById("start-btn").addEventListener("click", startQuiz);
 };
 
-
-// Define a timer function
-    // Starts when start quiz button is clicked
-    // Countdown from 75 seconds
-    // Subtracts 5 seconds if user answers question wrong
-    // When time reaches 0 call game over function
-
-// Define a game over function
-    // Called if all questions are answered 
-    // OR called if timer reaches zero
-    // Display final score
-    // Allow user to saves initials and score
-
-
-// Call initialze function
+// calls initialze function to start program
 init();
 
 
