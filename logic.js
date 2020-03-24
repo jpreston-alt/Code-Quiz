eed9b955aa2e1886362b09c38ff783e5a63fa397
Joanna Preston <joannappreston@gmail.com>
2020-03-24 14:51:40 -0700

    added finished function that stops timer, pulls up finished card, records score

--- logic.js
+++ logic.js
@@ -5,45 +5,57 @@
     -total score
     -variables to hold DOM elements
 - Create object instances holding quiz questions
-- Create a function that displays quiz questions on the UI
+- Create a function that creates a new question card element
+- Create a function to remove quiz card element
+- Create a function that displays quiz questions on the UI using new question card element
+- Create a function that moves the user on to the next question if there are any questions left to ask
+    - call remove element function to remove previously answered question card to make room for the question card that's created next
 - Create a function that checks if the user answered the question correctly
     - If correct
         - Display "correct"
         - Add to score
-        - Next question
+        - Call next question function
     - If incorrect
         - Display "incorrect"
         - Subract 5 seconds from the timer
-- Create a function that takes user to the next question
-    - displays next questions
-    - checks if their answer is correct
+- Create an ask question function that:
+    - Calls the create element function
+    - Calls the display question function
+    - Calls the check if correct function
 - Create an initialize function that starts the program over: 
     - displays intro card
     - resets score to zero
+    - resets timer to 75
     - when start button is clicked quiz is started by calling start quiz function
-    - resets timer to 75 seconds
-- Create a function that starts the quiz by hiding the intro card and displaying the quiz card
+- Create a start quiz function handler that 
+    - hides the intro card
+    - starts the timer
+    asks the first question
 - Create a timer function
-    - called in startQuiz function to start timer
     - Countdown from 75 seconds
     - Subtracts 5 seconds if user answers question wrong
-    - When time reaches 0 call game over function
-- Create a game over function
+    - When time reaches 0 call finsihed function
+- Create a finished function
     - Called if all questions are answered
     - OR called if timer reaches zero
     - Display final score
+    - Allow user to input their initials
+    - Sav score and input to high score
 - High score
-    - high score clickable
-    - brings user to high score page
-    - allows user to save initials and score
+    - high score link brings user to high score card
+    - user views list of high scores
+    - go back button
+    - clear high scores button
 - Call initialize function to start program
 - Allow user to store their highscore in local storage
 */
 
-var totalScore;
-var secondsLeft = 75;
+
+var secondsLeft;
 var timerEl = document.getElementById("timer");
 var introCardEl = document.getElementById("intro-card");
+var finalScoreEl = document.getElementById("final-score");
+var finishedCardEl = document.getElementById("finished-card");
 
 // Function constructor for Question object
 function Question(ask, answers, corrAns) {
@@ -86,16 +98,14 @@ var q5 = new Question(
 // array of question object instances
 var qArr = [q1, q2, q3, q4, q5];
 
-
-
 // function for creating new question card element
 function addElement() {
-    var newCard = document.createElement("div");
-    newCard.innerHTML = '<div class="card text-left col-12 col-sm-10 col-md-9 col-lg-6" id = "quiz-card"><div><h2 class="card-title" id="question">Question will go here.</h2><ul><li><button class="btn" id="ans1">A: Answer 1</button></li><li><button class="btn" id="ans2">B: Answer 2</button></li><li><button class="btn" id="ans3">C: Answer 3</button></li><li><button class="btn" id="ans4">D: Answer 4</button></li></ul><div><p><hr><span id="correct-or-not"></span></p></div></div></div>'
-    document.body.appendChild(newCard);
+    var newQuestionCard = document.createElement("div");
+    newQuestionCard.innerHTML = '<div class="card text-left col-12 col-sm-10 col-md-9 col-lg-6" id = "quiz-card"><div><h2 class="card-title" id="question">Question will go here.</h2><ul><li><button class="btn" id="ans1">A: Answer 1</button></li><li><button class="btn" id="ans2">B: Answer 2</button></li><li><button class="btn" id="ans3">C: Answer 3</button></li><li><button class="btn" id="ans4">D: Answer 4</button></li></ul><div><p><hr><span id="correct-or-not"></span></p></div></div></div>'
+    document.body.appendChild(newQuestionCard);
 }
 
-// function for displaying questions and answers on newly created quiz card element
+// function for displaying questions and answers on newly created question card element
 function displayQuestion(question) {
     document.getElementById("question").textContent = question.ask;
     for (var i = 0; i < question.answers.length; i++) {
@@ -103,21 +113,23 @@ function displayQuestion(question) {
     }
 };
 
-// function for removing quiz card element after it has been answered
+// function for removing question card element after it has been answered
 function removeElement() {
-    var newCard = document.getElementById("quiz-card");
-    newCard.parentNode.removeChild(newCard);
+    var newQuestionCard = document.getElementById("quiz-card");
+    newQuestionCard.parentNode.removeChild(newQuestionCard);
 }
 
-// function for moving on to the next question or ending the game if there are not more questions left. removes quiz card element before moving on to next question and creating a new element
+// function for moving on to the next question or ending the game if there are no more questions left. removes quiz card element before moving on to next question and creating a new element
 function nextQuestion(question) {
-    if ((qArr.indexOf(question) + 1) < qArr.length) {
-        removeElement();
-        quizQuestion(qArr[qArr.indexOf(question) + 1]);
-    } else {
-        console.log("quiz is over!")
-        // gameOver();
-    }
+    setTimeout(function(){
+        if ((qArr.indexOf(question) + 1) < qArr.length) {
+            removeElement();
+            quizQuestion(qArr[qArr.indexOf(question) + 1]);
+        } else {
+            console.log("quiz is over!")
+            finished();
+        }
+    }, 500)
 };
 
 // checks if answer is correct when an answer is clicked on.
@@ -131,8 +143,6 @@ function checkCorrect(question) {
             console.log(event.target.textContent)
             if (event.target.textContent === question.corrAns) {
                 displayCorrEl.textContent = "Correct!";
-                totalScore++;
-                console.log(totalScore);
                 nextQuestion(question);
             } else {
                 displayCorrEl.textContent = "Wrong!";
@@ -149,7 +159,6 @@ function quizQuestion(question) {
     checkCorrect(question);
 };
 
-
 // displays quiz card, hides intro card, starts quiz
 function startQuiz() {
     introCardEl.classList.add("display-none");
@@ -159,7 +168,7 @@ function startQuiz() {
 
 // defines initialize function
 function init() {
-    totalScore = 0;
+    secondsLeft = 25;
     document.getElementById("start-btn").addEventListener("click", startQuiz);
 };
 
@@ -167,23 +176,28 @@ function init() {
 //     - Countdown from 75 seconds
 //     - Subtracts 5 seconds if user answers question wrong
 //     - When time reaches 0 call game over function
-
+var timer;
 function startTimer() {
-    var timer = setInterval(function () {
+    timer = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = secondsLeft;
 
-        if (secondsLeft === 0) {
+        if (secondsLeft <= 0) {
             clearInterval(timer);
-            console.log("game over!");
-            // gameOver();
+            finished();
         }
-
     }, 1000);
+
 };
 
-function gameOver() {
-    
+function finished() {
+    finishedCardEl.classList.remove("display-none");
+    var quizCardEl = document.getElementById("quiz-card");
+    quizCardEl.classList.add("display-none");
+    finalScoreEl.textContent = secondsLeft;
+    clearInterval(timer);
+
+
 };
 
 // calls initialze function to start program
